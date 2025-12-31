7c5af024ecad7e5470398f986ba5c00ad5e25f9b
Patrick Graf <pats.tech@proton.me>
2025-12-31 18:58:22 +0100

    Create bot

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import os
+import discord
+from dotenv import load_dotenv
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv()
+
+class RecapBot(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user.name}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+    async def on_voice_state_update(self, member, before, after):
+        print(f'Member {member.name} has joined voice channel {after.channel} from {before.channel}')
+        print(f'Member: {member}')
+        print(f'Before: {before}')
+        print(f'After: {after}')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+intents = discord.Intents.default()
+intents.voice_states = True
+intents.message_content = True
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+client = RecapBot(intents=intents)
+client.run(os.getenv('BOT_TOKEN'))
