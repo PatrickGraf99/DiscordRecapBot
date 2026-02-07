206b326add6b30b54daee22b106dd364048b5e6e
Patrick Graf <pats.tech@proton.me>
2026-02-07 03:34:39 +0100

    Udate guild metastuff
    Add helper methods for logging, move some stuff

--- data_handler.py
+++ data_handler.py
@@ -1,8 +1,7 @@
+import json
 import os
 import logging
-from enum import Enum
-
-import discord
+import time
 
 logger = logging.getLogger('ServerRecapBot.data')
 
@@ -18,15 +17,18 @@ class DataHandler:
                                         'channel_id,channel_name,session_type\n')
         self.SESSION_LOG_FILENAME: str = 'session_log.csv'
         self.EVENT_LOG_FILENAME: str = 'event_log.csv'
+        self.GUILD_EVENTS_FILENAME: str = 'guild_events.jsonl'
+        self.GUILD_METADATA_SNAPSHOT_FILENAME: str = 'guild_snapshot.json'
+        self.json_schema_version: int = 1
 
     def ensure_guild_files_exist(self, guild_id: str) -> None:
         guild_dir = os.path.join(self.DATA_PATH, guild_id)
         if not os.path.exists(guild_dir):
             os.mkdir(guild_dir)
-        event_log_file = os.path.join(guild_dir, 'event_log.csv')
-        session_log_file = os.path.join(guild_dir, 'session_log.csv')
-        metadata_event_file = os.path.join(guild_dir, 'metadata_events.jsonl')
-        metadata_snapshot_file = os.path.join(guild_dir, 'metadata_snapshot.json')
+        event_log_file = os.path.join(guild_dir, self.EVENT_LOG_FILENAME)
+        session_log_file = os.path.join(guild_dir, self.SESSION_LOG_FILENAME)
+        metadata_event_file = os.path.join(guild_dir, self.GUILD_EVENTS_FILENAME)
+        metadata_snapshot_file = os.path.join(guild_dir, self.GUILD_METADATA_SNAPSHOT_FILENAME)
         if not os.path.exists(event_log_file):
             with open(event_log_file, 'w') as file:
                 file.write(self.EVENT_LOG_HEADER)
@@ -55,4 +57,37 @@ class DataHandler:
         session_log_path = os.path.join(self.DATA_PATH, str(guild_id), self.SESSION_LOG_FILENAME)
         with open(session_log_path, 'a') as session_log:
             session_log.write(session_csv_string)
+
+    def __append_guild_metadata(self, timestamp: float, guild_id: int, guild_event_type: str, payload: dict) -> None:
+        json_object = {'schema_version': self.json_schema_version, 'timestamp': timestamp,
+                       'guild_event': guild_event_type, 'guild_id': guild_id, 'payload': payload}
+        filename: str = os.path.join(self.DATA_PATH, str(guild_id), self.GUILD_EVENTS_FILENAME)
+        with open(filename, 'a') as event_file:
+            json.dump(json_object, event_file)
+            event_file.write('\n')
+
+    def log_guild_channel_add(self, timestamp: float, guild_id: int, guild_event_type: str, channel_id: int,
+                              channel_name: str, channel_category_id: int, channel_type: str) -> None:
+        pass
+
+    def log_guild_channel_remove(self, timestamp: float, guild_id: int, guild_event_type: str, channel_id: int,
+                                 channel_name: str, channel_category_id: int, channel_type: str) -> None:
+        pass
+
+    def log_guild_channel_rename(self, timestamp: float, guild_id: int, guild_event_type: str, channel_id: int,
+                                 channel_name_old: str, channel_name_new: str, channel_category_id: int,
+                                 channel_type: str) -> None:
+        pass
+
+    def log_guild_member_join(self, timestamp: float, guild_id: int, guild_event_type: str, member_id: int,
+                              member_name: str) -> None:
+        pass
+
+    def log_guild_member_remove(self, timestamp: float, guild_id: int, guild_event_type: str, member_id: int,
+                                member_name: str) -> None:
+        pass
+
+    def log_guild_rename(self, timestamp: float, guild_id: int, guild_event_type: str, member_id: int,
+                         guild_name_old: str, guild_name_new: str) -> None:
+        pass
 
